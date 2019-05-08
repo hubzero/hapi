21a945a6b9ebab0bd515ad57cd2502c01e82e777
Erich Huebner <erich.w.huebner@gmail.com>
2019-05-08 14:20:33 -0400

    new version of gdal

---
+++ scripts/gdal-2.4.0_install_deb8.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+# show commands being run
+set -x
+
+# Fail script on error.
+set -e
+
+pkgname=gdal
+VERSION=2.4.0
+basedir=/apps/share64/debian8
+environdir=${basedir}/environ.d
+pkginstalldir=${basedir}/${pkgname}
+tarinstalldir=${pkginstalldir}/tars
+installprefix=${pkginstalldir}/${VERSION}
+tarfilebase=${pkgname}-${VERSION}
+tarfilename=${tarfilebase}.tar.gz
+downloaduri=http://download.osgeo.org/gdal/${VERSION}/${tarfilename}
+script=$(readlink -f ${0})
+installdir=$(dirname ${script})
+
+if [[ ! -d ${pkginstalldir}/tars ]] ; then
+    mkdir -p ${pkginstalldir}/tars
+fi
+cd ${pkginstalldir}/tars
+
+if [[ ! -e ${tarfilename} ]] ; then
+    wget ${downloaduri}
+fi
+tar xvzf ${tarfilename}
+cd ${tarfilebase}
+./configure --prefix=${installprefix}
+make clean
+make
+make install
+
+if [[ ! -d ${environdir} ]] ; then
+    mkdir -p ${environdir}
+fi
+
+cat <<- _END_ > ${environdir}/${pkgname}-${VERSION}
+conflict GDAL_CHOICE
+
+desc "A translator library for raster and vector geospatial data formats"
+
+help "http://www.gdal.org/"
+
+version=${VERSION}
+location=${pkginstalldir}/\${version}
+
+setenv GDAL_INCLUDE_DIR \${location}/include
+setenv GDAL_LIB_DIR \${location}/lib
+setenv GDAL_DATA \${location}/share/gdal
+
+prepend LD_LIBRARY_PATH \${location}/lib
+prepend PATH \${location}/bin
+
+
+tags DEVEL
+_END_
